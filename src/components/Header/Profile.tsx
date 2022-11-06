1bc95cb9aabeaf3684d22cd59a476840837ce15f
joaopaulo33 <joaopaulo33573@gmail.com>
2022-11-06 15:26:12 -0300

    Diminuindo componentes e criando outros

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,21 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="3">
-        <Text> João Paulo Cardoso</Text>
-        <Text color="gray.300" fontSize="small">
-          joaopaulo33573@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="3">
+          <Text> João Paulo Cardoso</Text>
+          <Text color="gray.300" fontSize="small">
+            joaopaulo33573@gmail.com
+          </Text>
+        </Box>
+      )}
+
       <Avatar
         size="md"
         name="João Paulo"
